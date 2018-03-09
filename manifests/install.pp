fc5750aef745a5c6ef4a95e534ad597b983f02c3
Alexander Ursu <alexander.ursu@gmail.com>
2018-03-09 09:59:28 +0100

    Added puppet server installation resource

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,8 +7,11 @@
 # @example
 #   include puppet::install
 class puppet::install (
-    String $agent_package_name  = $puppet::params::agent_package_name,
-    String $puppet_agent_ensure = $puppet::puppet_agent_ensure,
+    String  $agent_package_name   = $puppet::params::agent_package_name,
+    String  $server_package_name  = $puppet::params::server_package_name,
+    String  $puppet_agent_ensure  = $puppet::puppet_agent_ensure,
+    String  $puppet_server_ensure = $puppet::puppet_server_ensure,
+    Boolean $puppet_master        = $puppet::master,
 ) inherits puppet::params
 {
     include puppet::repo
@@ -17,6 +20,14 @@ class puppet::install (
         ensure  => 'latest',
         name    => $agent_package_name,
         require => Package['puppet5-repository'],
+    }
+
+    if $puppet_master {
+        package { 'puppet-server':
+            ensure  => $puppet_server_ensure,
+            name    => $server_package_name,
+            require => Package['puppet-agent'],
+        }
     }
 }
 
