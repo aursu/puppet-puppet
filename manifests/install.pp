c48c883b5ad98fb68f3619d3a5279270fa68695b
Alexander Ursu <alexander.ursu@gmail.com>
2018-03-09 10:15:20 +0100

    Added r10k installation

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,11 +7,15 @@
 # @example
 #   include puppet::install
 class puppet::install (
-    String  $agent_package_name   = $puppet::params::agent_package_name,
-    String  $server_package_name  = $puppet::params::server_package_name,
-    String  $puppet_agent_ensure  = $puppet::puppet_agent_ensure,
-    String  $puppet_server_ensure = $puppet::puppet_server_ensure,
-    Boolean $puppet_master        = $puppet::master,
+    String  $agent_package_name  = $puppet::params::agent_package_name,
+    String  $agent_version       = $puppet::agent_version,
+    Boolean $puppet_master       = $puppet::master,
+    String  $server_package_name = $puppet::params::server_package_name,
+    String  $server_version      = $puppet::server_version,
+    Boolean $r10k                = $puppet::r10k_deployment,
+    String  $r10k_package_name   = $puppet::params::r10k_package_name,
+    String  $gem_path            = $puppet::params::gem_path,
+    String  $r10k_path           = $puppet::params::r10k_path,
 ) inherits puppet::params
 {
     include puppet::repo
@@ -24,10 +28,17 @@ class puppet::install (
 
     if $puppet_master {
         package { 'puppet-server':
-            ensure  => $puppet_server_ensure,
+            ensure  => $server_version,
             name    => $server_package_name,
             require => Package['puppet-agent'],
         }
     }
-}
 
+    if $r10k_deployment {
+        exec { 'r10k-installation':
+            command => "${gem_path} install ${r10k_package_name}",
+            creates => $r10k_path,
+            require => Package['puppet-agent'],
+        }
+    }
+}
