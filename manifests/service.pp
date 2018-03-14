311c1cb6596d553a3cc95186cf068b9d024daaac
Alexander Ursu <alexander.ursu@gmail.com>
2018-03-14 11:36:46 +0100

    Added class puppet::service for control puppetserver service run

---
+++ manifests/service.pp
@@ -0,0 +1,10 @@
+# puppet::service
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include puppet::service
+class puppet::service {
+}
