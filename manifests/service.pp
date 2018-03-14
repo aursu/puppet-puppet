8b77ae36a11d0d936cbadc8b2ad4b23cbbe62c55
Alexander Ursu <alexander.ursu@gmail.com>
2018-03-14 11:51:30 +0100

    Added logic into puppet::service

--- manifests/service.pp
+++ manifests/service.pp
@@ -6,5 +6,21 @@
 #
 # @example
 #   include puppet::service
-class puppet::service {
+class puppet::service (
+    Boolean $puppet_master          = $puppet::master,
+    String  $service_name           = $puppet::params::service_name,
+    String  $server_service_ensure  = $puppet::server_service_ensure,
+    Boolean $server_service_enable  = $puppet::server_service_enable,
+) inherits puppet::params
+{
+    include puppet::install
+
+    if $puppet_master {
+        service { 'puppet-server':
+            name    => $service_name,
+            ensure  => $server_service_ensure,
+            enable  => $server_service_enable,
+            require => Package['puppet-server']
+        }
+    }
 }
