00ae61577475692d86892d8c0e63b31583d4f202
Alexander Ursu <alexander.ursu@gmail.com>
2018-03-16 16:57:39 +0100

    Added class puppet::config for puppet.conf management

---
+++ manifests/config.pp
@@ -0,0 +1,10 @@
+# puppet::config
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include puppet::config
+class puppet::config {
+}
