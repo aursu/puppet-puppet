20bc0a16d5fcd053d21b37d22415d0287714f396
Alexander Ursu <alexander.ursu@gmail.com>
2018-03-20 17:36:50 +0100

    puppet::service is server related functionality

--- manifests/service.pp
+++ manifests/service.pp
@@ -7,20 +7,17 @@
 # @example
 #   include puppet::service
 class puppet::service (
-    Boolean $puppet_master          = $puppet::master,
     String  $service_name           = $puppet::params::service_name,
     String  $server_service_ensure  = $puppet::server_service_ensure,
     Boolean $server_service_enable  = $puppet::server_service_enable,
 ) inherits puppet::params
 {
-    include puppet::install
+    include puppet::install::server
 
-    if $puppet_master {
-        service { 'puppet-server':
-            name    => $service_name,
-            ensure  => $server_service_ensure,
-            enable  => $server_service_enable,
-            require => Package['puppet-server']
-        }
+    service { 'puppet-server':
+        name    => $service_name,
+        ensure  => $server_service_ensure,
+        enable  => $server_service_enable,
+        require => Package['puppet-server']
     }
 }
