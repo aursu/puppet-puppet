5f91a318e1616f79806c75865364dd78b3614e9a
Alexander Ursu <alexander.ursu@gmail.com>
2018-03-20 16:25:34 +0100

    Added puppet::install::r10k installation class

---
+++ manifests/install/r10k.pp
@@ -0,0 +1,10 @@
+# puppet::install::r10k
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include puppet::install::r10k
+class puppet::install::r10k {
+}
