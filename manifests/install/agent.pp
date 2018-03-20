4ff274af4242012a36763db9c3af766929c5dd11
Alexander Ursu <alexander.ursu@gmail.com>
2018-03-20 16:14:57 +0100

    Added classes puppet::install::agent and puppet::install::server

---
+++ manifests/install/agent.pp
@@ -0,0 +1,10 @@
+# puppet::install::agent
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include puppet::install::agent
+class puppet::install::agent {
+}
---
+++ manifests/install/server.pp
@@ -0,0 +1,10 @@
+# puppet::install::server
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include puppet::install::server
+class puppet::install::server {
+}
