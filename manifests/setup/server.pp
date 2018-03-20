ce965f8b1a6c4bd4336e4b45177c3e2026100157
Alexander Ursu <alexander.ursu@gmail.com>
2018-03-20 16:36:05 +0100

    Added server management class puppet::setup::server

---
+++ manifests/setup/server.pp
@@ -0,0 +1,10 @@
+# puppet::setup::server
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include puppet::setup::server
+class puppet::setup::server {
+}
