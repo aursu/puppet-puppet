6d5475cbd9796bb0459d202c487256d9d88b5ebb
Alexander Ursu <alexander.ursu@gmail.com>
2018-03-21 16:12:54 +0100

    Added puppet::enc to handle External Node Classifiers

---
+++ manifests/enc.pp
@@ -0,0 +1,10 @@
+# puppet::enc
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include puppet::enc
+class puppet::enc {
+}
