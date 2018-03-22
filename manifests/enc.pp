31e6a0f9e527c7240cc0f08326c5043f884f8fb1
Alexander Ursu <alexander.ursu@gmail.com>
2018-03-22 14:29:17 +0100

    Added ENC implementation

--- manifests/enc.pp
+++ manifests/enc.pp
@@ -6,5 +6,27 @@
 #
 # @example
 #   include puppet::enc
-class puppet::enc {
+class puppet::enc (
+    Boolean $use_enc            = $puppet::use_enc,
+
+    String  $enc_template       = $puppet::enc_template,
+    Optional[Stdlib::Absolutepath]
+            $enc_data_source    = $puppet::enc_data_source,
+    String  $enc_envname        = $puppet::enc_envname,
+    Stdlib::Absolutepath
+            $ruby_path          = $puppet::params::ruby_path,
+    Stdlib::Absolutepath
+            $external_nodes     = $puppet::params::external_nodes,
+) inherits puppet::params
+{
+    include puppet::service
+
+    file { $external_nodes:
+        content => template($enc_template),
+        mode    => '0755',
+        owner   => 'root',
+        group   => 'root',
+        alias   => 'enc-script',
+        before  => Service['puppet-server']
+    }
 }
