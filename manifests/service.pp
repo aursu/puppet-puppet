e50376b5b0825aa05ee582bfb91b7f29f38cf3d7
Alexander Ursu <alexander.ursu@gmail.com>
2018-03-22 15:35:23 +0100

    Changed formating

--- manifests/service.pp
+++ manifests/service.pp
@@ -7,9 +7,9 @@
 # @example
 #   include puppet::service
 class puppet::service (
-    String  $service_name           = $puppet::params::service_name,
     String  $server_service_ensure  = $puppet::server_service_ensure,
     Boolean $server_service_enable  = $puppet::server_service_enable,
+    String  $service_name           = $puppet::params::service_name,
 ) inherits puppet::params
 {
     include puppet::install::server
@@ -17,7 +17,7 @@ class puppet::service (
     service { $service_name:
         ensure  => $server_service_ensure,
         enable  => $server_service_enable,
-        require => Package['puppet-server'],
         alias   => 'puppet-server',
+        require => Package['puppet-server'],
     }
 }
