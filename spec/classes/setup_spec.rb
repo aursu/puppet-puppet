5d47ef39c532e346de89710257fe638897177f9e
Alexander Ursu <alexander.ursu@gmail.com>
2018-05-08 18:01:54 +0200

    Passet pdk test unit

--- spec/classes/setup_spec.rb
+++ spec/classes/setup_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe 'puppet::setup' do
+  let(:pre_condition) { 'include puppet' }
+
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
