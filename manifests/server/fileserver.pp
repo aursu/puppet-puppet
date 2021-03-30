6ebcc7cf5485c8cb18d9f6bb88d789771803ea31
Alexander Ursu <alexander.ursu@gmail.com>
2021-03-30 02:40:01 +0200

    TODO: file serving setup

---
+++ manifests/server/fileserver.pp
@@ -0,0 +1,11 @@
+# @summary Define file serving from Puppet server
+#
+# Define file serving from Puppet server
+#
+# [Adding file server mount points](https://puppet.com/docs/puppet/7.5/file_serving.html)
+# [fileserver.conf: Custom fileserver mount points](https://puppet.com/docs/puppet/7.5/config_file_fileserver.html)
+#
+# @example
+#   include puppet::server::fileserver
+class puppet::server::fileserver {
+}
