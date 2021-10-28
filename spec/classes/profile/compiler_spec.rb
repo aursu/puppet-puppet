a099aa39f0ddb9d01f40c515312d5428972fac74
Alexander Ursu <alexander.ursu@gmail.com>
2021-10-28 03:50:27 +0200

    Fixed unit test

--- spec/classes/profile/compiler_spec.rb
+++ spec/classes/profile/compiler_spec.rb
@@ -6,6 +6,11 @@ describe 'puppet::profile::compiler' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:params) do
+        {
+          ca_server: 'puppet',
+        }
+      end
 
       it { is_expected.to compile }
     end
