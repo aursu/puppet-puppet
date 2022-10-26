61de7c96a5c9410bc3e3b6ae76261cdfa4aec3a6
Alexander Ursu <alexander.ursu@gmail.com>
2022-10-26 10:49:36 +0200

    Ability to pass puppetdb server name into compiler profile

--- manifests/profile/compiler.pp
+++ manifests/profile/compiler.pp
@@ -18,24 +18,27 @@ class puppet::profile::compiler (
             $common_envname             = undef,
 
     Boolean $use_puppetdb               = true,
+    Stdlib::Host
+            $puppetdb_server            = 'puppet',
 )
 {
   class { 'puppet::profile::server':
-    sameca         => false,
-    puppetdb_local => false,
-    postgres_local => false,
+    sameca          => false,
+    puppetdb_local  => false,
+    postgres_local  => false,
 
-    platform_name  => $platform_name,
+    platform_name   => $platform_name,
 
-    server         => $server,
-    use_puppetdb   => $use_puppetdb,
+    server          => $server,
+    use_puppetdb    => $use_puppetdb,
+    puppetdb_server => $puppetdb_server,
 
-    hosts_update   => false,
+    hosts_update    => false,
 
-    use_common_env => $use_common_env,
-    common_envname => $common_envname,
+    use_common_env  => $use_common_env,
+    common_envname  => $common_envname,
 
-    ca_server      => $ca_server,
+    ca_server       => $ca_server,
   }
   contain puppet::profile::server
 }
