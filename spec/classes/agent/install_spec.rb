fcba3614fffe0240bf43693a760ca0d6ebb12808
Oleksandr Ursu <alexander.ursu@gmail.com>
2023-11-19 20:30:15 +0100

    Added unit test for RPM packages name compilation

--- spec/classes/agent/install_spec.rb
+++ spec/classes/agent/install_spec.rb
@@ -8,6 +8,30 @@ describe 'puppet::agent::install' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+
+      it {
+        is_expected.to contain_package('puppet-agent')
+          .with_name('puppet-agent')
+          .with_ensure('latest')
+      }
+
+      context 'check version setup' do
+        let(:params) do
+          {
+            agent_version: '7.26.0',
+          }
+        end
+
+        case os_facts[:os]['name']
+        when 'Rocky', 'CentOS'
+          os_version = os_facts[:os]['release']['major']
+          it {
+            is_expected.to contain_package('puppet-agent')
+              .with_name('puppet-agent')
+              .with_ensure("7.26.0-1.el#{os_version}")
+          }
+        end
+      end
     end
   end
 end
