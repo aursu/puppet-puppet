f53a085e35dfb839c61033d6fc6d815d08ddb0e0
Oleksandr Ursu <alexander.ursu@gmail.com>
2023-11-20 16:32:25 +0100

    Tidy up bootstrap feature

--- bootstrap/manifests/site.pp
+++
@@ -1,6 +0,0 @@
-# ENC script for unknown hosts returns "production" environment, therefore
-# puppet agent will install Puppet server on unknown host. To prevent this, add
-# condition
-if $::serverip == $::networking['ip'] {
-  include role::puppet::master
-}
