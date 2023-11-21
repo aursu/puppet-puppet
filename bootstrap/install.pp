7f19ecc5dd1033695f8117a127c563d1735a2b08
Oleksandr Ursu <alexander.ursu@gmail.com>
2023-11-21 08:24:02 +0100

    Declare puppet class

--- bootstrap/install.pp
+++ bootstrap/install.pp
@@ -1,3 +1,5 @@
+include puppet
+
 class { 'puppet::server::bootstrap::globals':
   access_data => lookup({ name => 'profile::puppet::deploy::access', default_value => [] }),
   ssh_config  => lookup({ name => 'profile::puppet::deploy::ssh_config', default_value => [] }),
