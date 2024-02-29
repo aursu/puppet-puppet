4908f83b6c3585a0db7a39cfb3184d6ed509f709
Oleksandr Ursu <alexander.ursu@gmail.com>
2024-02-29 20:52:22 +0100

    Added bsys as dependency to puppet module

--- plans/agent/install.pp
+++ plans/agent/install.pp
@@ -2,5 +2,6 @@ plan puppet::agent::install (
   TargetSpec $targets,
   Puppet::Platform $collection = 'puppet8',
 ) {
-  return run_task('puppet_agent::install', $targets, collection => $collection, stop_service => true)
+  run_task(bsys::install_yum, $targets)
+  return run_task(puppet_agent::install, $targets, collection => $collection, stop_service => true)
 }
