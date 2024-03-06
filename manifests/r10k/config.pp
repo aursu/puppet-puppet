0e3c074e723283d31b1f39f8d4424866a22e32b5
Oleksandr Ursu <alexander.ursu@gmail.com>
2024-03-06 16:38:05 +0100

    Added comments to puppet::r10k::config

--- manifests/r10k/config.pp
+++ manifests/r10k/config.pp
@@ -1,23 +1,61 @@
-# @summary A short summary of the purpose of this class
+# @summary Default r10k configuration file
 #
-# r10k configuration 
+# The r10k configuration file is created only if r10k.yaml does not already
+# exist. By default, it establishes the `production` environment. Optionally,
+# it can also configure `common` and `enc` environments.
 #
 # @param r10k_config_setup
-#   Decide whether to update the R10K configuration file each time it is required, or keep it to be
-#   created only when it does not exist.
+#   Determines the management strategy for the r10k configuration file: whether
+#   to update it on each notify event or to only create it if it does not
+#   already exist.
+#
+# @param r10k_yaml_template
+#   Allows the specification of a custom r10k.yaml ERB (Embedded Ruby) template for setup,
+#   instead of using the default template provided.
+#
+# @param cachedir
+#   This parameter allows for the customization of the r10k cache directory path,
+#   aligning with the `cachedir` parameter specified inside the r10k configuration.
+#
+# @param environmentpath
+#   Specifies the directory path to Puppet environments. This parameter
+#   corresponds to the `basedir` parameter for each source defined in the r10k
+#   configuration.
+#
+# @param production_remote
+#   Sets the Git repository URL for the `production` environment. This setting aligns
+#   with the `remote` parameter designated for the `production` source in the r10k
+#   configuration. The default URL is https://github.com/aursu/control-init.git.
+#
+# @param use_common_env
+#   Determines whether to setup the `common` environment.
+#
+# @param common_remote
+#   Sets the Git repository URL for the `common` environment. This setting aligns
+#   with the `remote` parameter designated for the `common` source in the r10k
+#   configuration. The default URL is https://github.com/aursu/control-common.git.
+#
+# @param use_enc
+#   Determines whether to setup the `enc` environment.
+#
+# @param enc_remote
+#   Sets the Git repository URL for the `enc` environment. This setting aligns
+#   with the `remote` parameter designated for the `enc` source in the r10k
+#   configuration. The default URL is https://github.com/aursu/control-enc.git.
 #
 # @example
 #   include puppet::r10k::config
+#
 class puppet::r10k::config (
   String  $r10k_yaml_template = $puppet::r10k_yaml_template,
   Stdlib::Absolutepath $cachedir = $puppet::params::r10k_cachedir,
   Stdlib::Absolutepath $environmentpath = $puppet::params::environmentpath,
+  Boolean $r10k_config_setup = $puppet::r10k_config_setup,
   String  $production_remote = $puppet::production_remote,
   Boolean $use_common_env = $puppet::use_common_env,
   String  $common_remote = $puppet::common_remote,
   Boolean $use_enc = $puppet::use_enc,
   String  $enc_remote = $puppet::enc_remote,
-  Boolean $r10k_config_setup = $puppet::r10k_config_setup,
 ) inherits puppet::params {
   include puppet::r10k::setup
 
