87ee69a3a805b0da511e3098aac1986af4962bfc
Oleksandr Ursu <alexander.ursu@gmail.com>
2024-04-09 05:25:13 +0200

    Added support for certname into PuppetDB bootstrap

--- bootstrap/bolt/plans/puppetdb.pp
+++ bootstrap/bolt/plans/puppetdb.pp
@@ -8,12 +8,14 @@ plan puppet_bootstrap::puppetdb (
   String $database_username = 'puppetdb',
   String $database_password = 'puppetdb',
   Boolean $manage_firewall = false,
+  Optional[String] $certname = undef,
 ) {
   # Unable to connect to puppetdb server (https://puppet-puppet-puppetdb-1:8081):
   # Server hostname 'puppet-puppet-puppetdb-1' did not match server certificate;
   # expected one of e7475213c9fe, DNS:e7475213c9fe
   run_plan( puppet_bootstrap::puppetdb::node, $targets,
     puppet_server => $puppet_server,
+    certname      => $certname,
     platform_name => $platform_name,
   )
 
--- bootstrap/bolt/plans/puppetdb/node.pp
+++ bootstrap/bolt/plans/puppetdb/node.pp
@@ -6,21 +6,25 @@
 plan puppet_bootstrap::puppetdb::node (
   TargetSpec $targets = 'puppetdb',
   Stdlib::Fqdn $puppet_server = 'puppet',
+  Optional[String] $certname = undef,
   Puppet::Platform $platform_name = 'puppet8',
 ) {
   # Submit a certificate request to the Puppet server
   run_plan( puppet::bootstrap, $targets,
     server     => $puppet_server,
+    certname   => $certname,
     collection => $platform_name,
   )
 
   # Sign certificate request on the Puppet server
   run_plan( puppet::cert::sign, $targets,
-    server => $puppet_server,
+    server   => $puppet_server,
+    certname => $certname,
   )
 
   # download certificate onto the PuppetDB node.
   run_plan( puppet::bootstrap, $targets,
-    server => $puppet_server,
+    server   => $puppet_server,
+    certname => $certname,
   )
 }
