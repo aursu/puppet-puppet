40cc0a0f75de517ea26325a6cbf216ad99f06a24
Alexander Ursu <alexander.ursu@gmail.com>
2024-04-12 17:00:59 +0200

    Added ability to set particular hostname

--- plans/agent/hostname.pp
+++ plans/agent/hostname.pp
@@ -7,15 +7,25 @@
 #
 plan puppet::agent::hostname (
   TargetSpec $targets,
+  Optional[Stdlib::Fqdn] $hostname = undef,
 ) {
+  if $hostname and get_targets($targets).size > 1 {
+    fail("You can set up the hostname \"${hostname}\" on only one target.")
+  }
+
   get_targets($targets).each |$target| {
+    $target_name = $hostname ? {
+      Stdlib::Fqdn => $hostname,
+      default      => $target.name,
+    }
+
     run_task(
       'puppet::hostname',
       $target,
-      "Set hostname on ${target.name}",
+      "Set hostname on ${target_name}",
       '_catch_errors' => true,
-      '_run_as' => 'root',
-      'hostname' => $target.name
+      '_run_as'       => 'root',
+      'hostname'      => $target_name,
     )
   }
 }
