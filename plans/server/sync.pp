f36c0630845004fe0e6d78cb4aea8178544be167
Alexander Ursu <alexanderu@crytek.de>
2024-08-15 15:59:27 +0200

    Updated comments

--- plans/server/sync.pp
+++ plans/server/sync.pp
@@ -1,16 +1,18 @@
-# @summary Clean node certificates on Puppet controller node
+# @summary Sync Puppet manifests on specified Puppet controller nodes
 #
-# Bolt plan which run r10k command for each Puppet
-# controller node as Bolt plan target
+# This Bolt plan runs the r10k command on each Puppet controller node specified in the target.
+# It ensures that the latest manifests are synced.
 #
 # @param targets
-#   Puppet server(s) where manifests should be synced
+#   Puppet server(s) where the manifests should be synced.
 #
 plan puppet::server::sync (
   TargetSpec $targets,
 ) {
-  run_plan(facts, $targets)
+  # Gather facts about the target nodes
+  run_plan('facts', $targets)
 
+  # Apply the puppet::r10k::run class on each target node
   return apply($targets) {
     include puppet
 
