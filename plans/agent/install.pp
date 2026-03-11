ea28d5f1b6ee84362492b4b72e9da90e72613c4d
Alexander Ursu <alexanderu@crytek.de>
2026-03-11 19:57:54 +0100

    Add support for puppet/openvox_bootstrap dependency and update install plan logic

--- plans/agent/install.pp
+++ plans/agent/install.pp
@@ -3,5 +3,15 @@ plan puppet::agent::install (
   Puppet::Platform $collection = 'puppet8',
 ) {
   run_task(bsys::install_yum, $targets)
-  return run_task(puppet_agent::install, $targets, collection => $collection, stop_service => true)
+  if $collection in ['puppet7', 'puppet8'] {
+    return run_task('puppet_agent::install', $targets,
+      collection   => $collection,
+      stop_service => true,
+    )
+  } else {
+    return run_task('openvox_bootstrap::install', $targets,
+      collection   => $collection,
+      stop_service => true,
+    )
+  }
 }
